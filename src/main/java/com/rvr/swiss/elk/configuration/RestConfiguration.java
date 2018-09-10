d8c210cf8094c83fced6e99f58669f30e478f193
Roman Rybak <r.romanv1@gmail.com>
2018-09-11 00:39:41 +0300

    add configuration for connection pooling

--- src/main/java/com/rvr/swiss/elk/configuration/RestConfiguration.java
+++ src/main/java/com/rvr/swiss/elk/configuration/RestConfiguration.java
@@ -7,6 +7,11 @@ import com.rvr.swiss.elk.dto.LogTestDTO;
 import com.rvr.swiss.elk.handlers.ServerResponseErrorHandler;
 import com.rvr.swiss.elk.integration.ElasticsearchClientImpl;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
@@ -29,6 +35,20 @@ public class RestConfiguration {
 
     private static final Logger LOG =  LoggerFactory.getLogger(ElasticsearchClientImpl.class);
 
+    // Determines the timeout in milliseconds until a connection is established.
+    private static final int CONNECT_TIMEOUT = 30000;
+
+    // The timeout when requesting a connection from the connection manager.
+    private static final int REQUEST_TIMEOUT = 30000;
+
+    // The timeout for waiting for data
+    private static final int SOCKET_TIMEOUT = 60000;
+
+
+    private static final int MAX_TOTAL_CONNECTIONS = 800;
+    private static final int MAX_TOTAL_PER_ROUTE_CONNECTIONS = 800;
+
+
     @Bean(name = "restTemplate")
     public RestTemplate restTemplate(@Autowired ObjectMapper mapper) {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
@@ -39,16 +59,48 @@ public class RestConfiguration {
                 new ObjectToStringHttpMessageConverter(new DefaultFormattingConversionService()),
                 jsonConverter));
 
-      //  HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-      //  factory.setReadTimeout(10000);
-      //  factory.setConnectTimeout(10000);
+        HttpComponentsClientHttpRequestFactory factory = clientHttpRequestFactory();
 
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+       // SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         BufferingClientHttpRequestFactory bufferingClientHttpRequestFactory =
-                new BufferingClientHttpRequestFactory(requestFactory);
-        requestFactory.setOutputStreaming(false);
+                new BufferingClientHttpRequestFactory(factory);
+        //requestFactory.setOutputStreaming(false);
         restTemplate.setRequestFactory(bufferingClientHttpRequestFactory);
         restTemplate.setErrorHandler(new ServerResponseErrorHandler(mapper));
         return restTemplate;
+    }
+
+    private HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(getHttpClient());
+        return requestFactory;
+    }
+
+
+    private CloseableHttpClient getHttpClient(){
+        return HttpClients.custom()
+                .setDefaultRequestConfig(getRequestConfig())
+                .setConnectionManager(poolingConnectionManager())
+                .setDefaultSocketConfig(getSocketConfig())
+                .build();
+    }
+
+    private SocketConfig getSocketConfig() {
+        return SocketConfig.custom().setSoKeepAlive(true).setTcpNoDelay(true).build();
+    }
+
+    private RequestConfig getRequestConfig() {
+        return RequestConfig.custom()
+                    .setConnectionRequestTimeout(REQUEST_TIMEOUT)
+                    .setConnectTimeout(CONNECT_TIMEOUT)
+                    .setSocketTimeout(SOCKET_TIMEOUT).build();
+    }
+
+    private PoolingHttpClientConnectionManager poolingConnectionManager(){
+        PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager();
+        poolingConnectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
+        poolingConnectionManager.setDefaultMaxPerRoute(MAX_TOTAL_PER_ROUTE_CONNECTIONS);
+        return poolingConnectionManager;
     }
 }
